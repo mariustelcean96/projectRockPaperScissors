5626fd71d595d820d63467407adbf1b5c3798abc
Telcean Marius-Andrei <marius.telcean96@gmail.com>
2022-01-19 22:26:46 +0200

    Upgrade to probeta version.

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
-/* The task is to create a (simple) Rock Paper Scissors game */
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+function userPlay(userSelection) {
+    return userSelection;
+}
+
+function computerPlay() {
+  const possibleChoices = ["rock", "paper", "scissors"];
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let computerSelection = possibleChoices[randomNumber - 1];
+  console.log("The PC chose: " + computerSelection);
+  return computerSelection;
+}
+
+const infoDiv = document.querySelector('.info');
+const para = document.createElement('p');
+infoDiv.appendChild(para);
+
+function showIntro() {
+  para.textContent = 'Intro intarziat.';
+}
+let timeoutID = setTimeout(showIntro, 5000);
+const score = document.querySelector('.score');
+const paraScore = document.createElement('p');
+score.appendChild(paraScore);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    para.textContent = '';
+    const computerSelection = computerPlay();
+    const userSelection = userPlay(e.target.value);
+    if (userSelection === "rock") {
+        if (computerSelection === "rock") {
+            para.textContent = 'The game is a tie.';
+            tieScore++;
+        } else if (computerSelection === "paper") {
+            para.textContent = 'You lose. Paper beats rock.' ;
+            computerScore++;
+        } else {
+            para.textContent = 'You won. Rock beats scissors.';
+            playerScore++;
+        }
+    } else if (userSelection === "paper") {
+      if (computerSelection === "rock") {
+        para.textContent = 'You won. Paper beats Rock.';
+        playerScore++;
+      } else if (computerSelection === "scissors") {
+        para.textContent = 'You lose. Scissors beat Paper.';
+        computerScore++;
+      } else {
+        tieScore++;
+        para.textContent = 'The game is a tie.';
+      }
+    } else if (userSelection === 'scissors') {
+      if (computerSelection === "paper") {
+        para.textContent = 'You won. Scissors beat Paper.';
+        playerScore++;
+      } else if (computerSelection === "rock") {
+        para.textContent = 'You lose. Rock beats Scissors.';
+        computerScore++;
+      } else {
+        tieScore++;
+        para.textContent = 'The game is a tie.';
+      }
+    }
+paraScore.textContent = 'PC:' + computerScore + ' ' + 'User:' + playerScore + ' ' + 'Tie:' + tieScore;
+  });
+});
+
+/* The task is to create a (simple) Rock Paper Scissors game 
 
 let playerScore = 0;
 let computerScore = 0;
@@ -71,4 +143,4 @@ function playRound(playerSelection, computerSelection) {
 const para = document.querySelector('p');
 para.textContent = "Computer: " + computerScore + '. ' + "Player: " + playerScore + '. ' + "Tie: " + tieScore;
 console.log("Computer final score is: " + computerScore);
-console.log("Player final score is: " + playerScore);
+console.log("Player final score is: " + playerScore); */
